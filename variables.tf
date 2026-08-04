5d706ddb8cc3373bedcec5b3ac98ba9cc5c0bff7
priyanshu-gairola <gairola.priyanshu03@gmail.com>
2026-08-04 21:24:14 +0530

    Used variables instead of hardcoding

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "ec2_instance_type" {
+  description = "Type of EC2 instance"
+  default     = "t3.micro"
+  type        = string
+}
+
+variable "ec2-ami" {
+  description = "AMI ID for the EC2 instance"
+  default     = "ami-0011550b539717e2a"
+  type        = string
+}
+
+variable "ec2_block_storage" {
+  description = "Block storage size for the EC2 instance"
+  default     = 10
+  type        = number
+}
